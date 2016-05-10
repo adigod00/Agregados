43eaee62204de8b0f608a4e8008b7f76260a1cbc
Alvaro Digon <adigod00@estudiantes.unileon.es>
2016-05-10 11:30:55 +0200

    Clases terminadas

---
+++ src/es/unileon/prg1/agregados/Almacen.java
@@ -0,0 +1,175 @@
+package es.unileon.prg1.agregados;
+
+/**
+ * Clase que representa un almacen de productos de capacidad
+ * limitada.
+ *
+ * @author PRG
+ * @version 1.0
+ */
+class Almacen{
+
+	/**
+	 * Matriz de productos que contiene el almacen
+	 */
+	private Producto productos[];
+	/**
+	 * Capacidad del almacen en tipos de productos distintos
+	 */
+	final int MAXIMO_PROD = 10;
+	/**
+	 * Numero de tipos de productos almacenados en cada momento
+	 * en el almacen (o primera posicion libre de la matriz de
+	 * productos)
+	 */
+	private int siguiente;
+
+	/**
+	 * Constructor de la clase que inicializa sus atributos
+	 */
+	Almacen(){
+		this.productos= new Producto[this.MAXIMO_PROD];
+		this.siguiente=0;
+		//COMPLETAR
+	}
+
+	/**
+	 * Metodo que sirve para aniadir un producto al almacen,
+	 * siempre que no este repetido y haya sitio.
+	 *
+	 * @param producto el producto que se desea aniadir
+	 * @return verdadero si el producto ya sido aniadido con exito
+	 * y falso en caso contrario
+	 */
+	boolean anyadir(Producto producto){
+		//COMPLETAR
+		StringBuffer salida=new StringBuffer();
+		boolean correcto=false;
+		if(existe(producto)== true){
+			salida.append("ERROR.El producto " + producto.obtenerNombre() + " ya existe.");
+		}else if(this.siguiente==MAXIMO_PROD){
+			salida.append("ERROR.No queda espacio en el almacén");
+		}else{
+			this.productos[this.siguiente++]=new Producto (producto);
+			correcto=true;
+		}
+		return correcto;
+	}
+
+	/**
+	 * Comprueba si un producto se encuentra ya en el almacen.
+	 *
+	 * @param producto el producto buscado
+	 * @return verdadero si el producto ya se encuentra en el almacen
+	 * y falso en caso contrario
+	 */
+	boolean existe(Producto producto){
+		//COMPLETAR
+		boolean existe=false;
+		if(buscar(producto.obtenerNombre())!= null){
+		existe=true;
+		}
+		return existe;
+	}
+
+	/**
+	 * Utiliza el algoritmo de la busqueda binaria sobre la
+	 * coleccion ordenada de los productos.
+	 *
+	 * @param nombre nombre del producto que se desea localizar
+	 * @return el producto buscado si se encuentra en el almacen
+	 * o nulo en caso contrario
+	 */
+	Producto buscar(String nombre){
+		//COMPLETAR
+			 Producto producto;
+			 int mitad, limiteInferior, limiteSuperior;
+			 mitad = 0;
+			 limiteInferior = 0;
+			 limiteSuperior = siguiente-1;
+			 producto = null;
+			 while ( (limiteInferior <= limiteSuperior) &&
+				 (producto == null)) {
+				 mitad = (limiteInferior + limiteSuperior) / 2 ; 
+					if (productos[mitad].esMenor(nombre)==true){
+						limiteInferior = mitad + 1;
+					} else if (productos[mitad].esMayor(nombre)==true) {
+						limiteSuperior = mitad - 1;
+					} else{
+						producto = productos[mitad];
+			 		}
+				 } 
+		return producto;
+	}
+
+	/**
+	 * Intercambia la posicion de dos productos dentro 
+	 * de la matriz.
+	 *
+	 * @param i posicion del primer producto
+	 * @param j posicion del segundo producto
+	 */
+	void cambiar(int i, int j){
+		//COMPLETAR
+		Producto temporal;
+		temporal=this.productos[i];
+		this.productos[i]=this.productos[j];
+		this.productos[j]=temporal;
+	}
+
+	/**
+	 * Utiliza el algoritmo de la burbuja bidireccional para 
+	 * ordenar los productos del almacen de manera ascente en orden
+	 * alfabetico por su nombre.
+	 */
+	void ordenar(){
+		int limiteSuperior, limiteInferior;
+		boolean fin, cambiado;
+
+		limiteSuperior = productos.length;
+		limiteInferior = -1;
+		fin = false;
+
+		while ( (limiteInferior < limiteSuperior) && !fin ) {
+			limiteInferior++;
+			limiteSuperior--;
+			cambiado = false;
+			for (int j = limiteInferior; j < limiteSuperior; j++){
+				if ( productos[j].esMayor(productos[j + 1]) ) {
+					cambiar(j,j+1);
+					cambiado = true;
+				}
+			}
+			if (!cambiado){
+				fin = true;
+			}else{
+				cambiado = false;
+				for (int j = limiteSuperior; --j >= limiteInferior; ) {
+					if ( productos[j].esMayor(productos[j + 1]) ) {
+						cambiar(j,j+1);
+						cambiado = true;
+					}
+				}
+				if (!cambiado) {
+					fin = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Devuelve una cadena de caracteres con los productos del almacen ordenados.
+	 */
+	public String toString(){
+		StringBuffer salida = new StringBuffer();
+		ordenar();
+		salida.append("---------");
+		salida.append("PRODUCTOS");
+		salida.append("---------");
+		for (int i=0; i < siguiente; i++){
+			salida.append(productos[i].toString());
+		}
+		return salida.toString();
+	}
+
+}
